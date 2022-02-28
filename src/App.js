c9d9f93dd4b3ba2792b59e91eb9b32eb72a75f90
sovisrushain <sovisrushain@gmail.com>
2022-02-28 11:01:34 +0530

    add Cart and Layout components

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import {Route, Routes} from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupsPage from './pages/NewMeetups';
 import FavouritesPage from './pages/Favourites';
-import MainNav from './components/MainNav';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
-    <div>
-      <MainNav />
+    <Layout>
       <Routes>
         <Route path='/' exact element={<AllMeetupsPage />}/>
         <Route path='/new-meetup' element={<NewMeetupsPage />}/>
         <Route path='/favourites' element={<FavouritesPage />}/>
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
